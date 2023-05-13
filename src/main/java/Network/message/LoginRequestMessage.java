349e8768d9440278d92d8a8e34ee7075e8979a92
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2023-05-13 19:00:31 +0200

    Modifica costruttore LoginRequestMessage

--- src/main/java/Network/message/LoginRequestMessage.java
+++ src/main/java/Network/message/LoginRequestMessage.java
@@ -1,10 +1,18 @@
 package Network.message;
 
+import Network.ClientSide.RemoteClientInterface;
+
 public class LoginRequestMessage extends Message {
     private static final long serialVersionUID = 7L;
+    private RemoteClientInterface client;
 
-    public LoginRequestMessage(String nickname) {
+    public LoginRequestMessage(RemoteClientInterface client, String nickname) {
         super(nickname, MessageEnumeration.LOGIN_REQUEST);
+        this.client = client;
+    }
+
+    public RemoteClientInterface getClient(){
+        return this.client;
     }
 
 }
